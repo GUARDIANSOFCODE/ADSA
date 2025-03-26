bb0d41a82dfa781e30812fe273ec978d3c15bb55
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-03-26 17:38:14 +0530

    Create Huffman Encoding.C

---
+++ Huffman Encoding.C
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_TREE_HT 100
+
+// Huffman Tree Node
+struct HuffmanNode {
+    char data;
+    unsigned freq;
+    struct HuffmanNode *left, *right;
+};
+
+// Min Heap structure
+struct MinHeap {
+    unsigned size;
+    unsigned capacity;
+    struct HuffmanNode** array;
+};
+
+// Function to create a new node
+struct HuffmanNode* createNode(char data, unsigned freq) {
+    struct HuffmanNode* node = (struct HuffmanNode*)malloc(sizeof(struct HuffmanNode));
+    node->data = data;
+    node->freq = freq;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Function to create a MinHeap
+struct MinHeap* createMinHeap(unsigned capacity) {
+    struct MinHeap* minHeap = (struct MinHeap*)malloc(sizeof(struct MinHeap));
+    minHeap->size = 0;
+    minHeap->capacity = capacity;
+    minHeap->array = (struct HuffmanNode**)malloc(minHeap->capacity * sizeof(struct HuffmanNode*));
+    return minHeap;
+}
+
+// Swap two MinHeap nodes
+void swapNodes(struct HuffmanNode** a, struct HuffmanNode** b) {
+    struct HuffmanNode* temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Heapify function
+void minHeapify(struct MinHeap* minHeap, int idx) {
+    int smallest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+
+    if (left < minHeap->size && minHeap->array[left]->freq < minHeap->array[smallest]->freq)
+        smallest = left;
+
+    if (right < minHeap->size && minHeap->array[right]->freq < minHeap->array[smallest]->freq)
+        smallest = right;
+
+    if (smallest != idx) {
+        swapNodes(&minHeap->array[smallest], &minHeap->array[idx]);
+        minHeapify(minHeap, smallest);
+    }
+}
+
+// Extract the minimum node from MinHeap
+struct HuffmanNode* extractMin(struct MinHeap* minHeap) {
+    struct HuffmanNode* temp = minHeap->array[0];
+    minHeap->array[0] = minHeap->array[minHeap->size - 1];
+    minHeap->size--;
+    minHeapify(minHeap, 0);
+    return temp;
+}
+
+// Insert a node into MinHeap
+void insertMinHeap(struct MinHeap* minHeap, struct HuffmanNode* node) {
+    int i = minHeap->size;
+    minHeap->size++;
+
+    while (i && node->freq < minHeap->array[(i - 1) / 2]->freq) {
+        minHeap->array[i] = minHeap->array[(i - 1) / 2];
+        i = (i - 1) / 2;
+    }
+
+    minHeap->array[i] = node;
+}
+
+// Build MinHeap
+void buildMinHeap(struct MinHeap* minHeap) {
+    int n = minHeap->size - 1;
+    for (int i = (n - 1) / 2; i >= 0; --i)
+        minHeapify(minHeap, i);
+}
+
+// Create Huffman Tree
+struct HuffmanNode* buildHuffmanTree(char data[], int freq[], int size) {
+    struct MinHeap* minHeap = createMinHeap(size);
+    for (int i = 0; i < size; ++i)
+        minHeap->array[i] = createNode(data[i], freq[i]);
+    
+    minHeap->size = size;
+    buildMinHeap(minHeap);
+
+    while (minHeap->size > 1) {
+        struct HuffmanNode *left = extractMin(minHeap);
+        struct HuffmanNode *right = extractMin(minHeap);
+        struct HuffmanNode *top = createNode('$', left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+        insertMinHeap(minHeap, top);
+    }
+    
+    return extractMin(minHeap);
+}
+
+// Print Huffman codes (Recursive)
+void printCodes(struct HuffmanNode* root, int arr[], int top) {
+    if (root->left) {
+        arr[top] = 0;
+        printCodes(root->left, arr, top + 1);
+    }
+    if (root->right) {
+        arr[top] = 1;
+        printCodes(root->right, arr, top + 1);
+    }
+    if (!root->left && !root->right) {
+        printf("%c: ", root->data);
+        for (int i = 0; i < top; i++)
+            printf("%d", arr[i]);
+        printf("\n");
+    }
+}
+
+// Huffman Encoding Function
+void HuffmanCodes(char data[], int freq[], int size) {
+    struct HuffmanNode* root = buildHuffmanTree(data, freq, size);
+    int arr[MAX_TREE_HT], top = 0;
+    printCodes(root, arr, top);
+}
+
+// Main function
+int main() {
+    char arr[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
+    int freq[] = { 5, 9, 12, 13, 16, 45 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Huffman Codes:\n");
+    HuffmanCodes(arr, freq, size);
+    return 0;
+}
